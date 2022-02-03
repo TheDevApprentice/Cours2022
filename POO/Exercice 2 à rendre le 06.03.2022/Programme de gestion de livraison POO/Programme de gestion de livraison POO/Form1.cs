f8045c37043dd334dc3fd12ecd84a3a3a6e0c1d2
Hugo Abric <80226527+TheDevApprentice@users.noreply.github.com>
2022-02-03 17:10:58 -0500

    jiojj,

---
+++ POO/Exercice 2 à rendre le 06.03.2022/Programme de gestion de livraison POO/Programme de gestion de livraison POO/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Programme_de_gestion_de_livraison_POO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
