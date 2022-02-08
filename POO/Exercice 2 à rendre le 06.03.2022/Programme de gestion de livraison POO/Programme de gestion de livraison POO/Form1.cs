6cd292e1eb00b095cfeb3a7ccd15710dbcea8d25
Hugo Abric <80226527+TheDevApprentice@users.noreply.github.com>
2022-02-08 13:33:41 -0500

    .

--- POO/Exercice 2 à rendre le 06.03.2022/Programme de gestion de livraison POO/Programme de gestion de livraison POO/Form1.cs
+++ POO/Exercice 2 à rendre le 06.03.2022/Programme de gestion de livraison POO/Programme de gestion de livraison POO/Form1.cs
@@ -12,6 +12,8 @@ namespace Programme_de_gestion_de_livraison_POO
 {
     public partial class Form1 : Form
     {
+        Voyage voyage; 
+
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +21,7 @@ namespace Programme_de_gestion_de_livraison_POO
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         private void btn_assigneLivraison_Click(object sender, EventArgs e)
