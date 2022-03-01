37616b7dcddd0985b89845e930a4a08cfc5c07ec
Hugo Abric <80226527+TheDevApprentice@users.noreply.github.com>
2022-03-01 15:43:18 -0500

    fiinition

--- POO/Exercice 2 à rendre le 06.03.2022/Programme de gestion de livraison POO/Programme de gestion de livraison POO/Form1.cs
+++ POO/Exercice 2 à rendre le 06.03.2022/Programme de gestion de livraison POO/Programme de gestion de livraison POO/Form1.cs
@@ -29,20 +29,20 @@ namespace Programme_de_gestion_de_livraison_POO
         BindingSource bindingSource3 = new BindingSource();
         BindingSource bindingSource4 = new BindingSource();
 
-
-
         public entreeLivraison EntreeLivraison { get => entreeLivraison; set => entreeLivraison = value; }
         public entreeCamion EntreeCamion { get => entreeCamion; set => entreeCamion = value; }
         public entreeCamionneur EntreeCamionneur { get => entreeCamionneur; set => entreeCamionneur = value; }
         public EntreeVoyage EntreeVoyage1 { get => EntreeVoyage; set => EntreeVoyage = value; }
+
         internal Voyage Voyage { get => voyage; set => voyage = value; }
         internal Voyage VoyageSelectionnée { get => voyageSelectionnée; set => voyageSelectionnée = value; }
+        internal Camion Camion { get => camion; set => camion = value; }
 
         internal List<livraison> ListeLivraisonNonAssignees { get => EntreeLivraison.ListeLivraisonNonAssignees; set => listeLivraisonNonAssignees = value; }
         internal List<Camionneur> ListeCamionneurs1 { get => EntreeCamionneur.ListeCamionneur; set => listeCamionneurs = value; }
         internal List<Voyage> ListeVoyages1 { get => EntreeVoyage.ListeVoyage; set => listeVoyages = value; }
         internal List<Camion> ListeCamions { get => EntreeCamion.ListeCamion; set => listeCamions = value; }
-        internal Camion Camion { get => camion; set => camion = value; }
+    
 
         public Form1()
         {
@@ -51,7 +51,6 @@ namespace Programme_de_gestion_de_livraison_POO
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
             bindingSource4.DataSource = ListeVoyages1;
             lst_voyages.DataSource = bindingSource4;
 
@@ -91,12 +90,10 @@ namespace Programme_de_gestion_de_livraison_POO
             ListeCamions.Add(new Camion(5000, 300));
             ListeCamions.Add(new Camion(2000, 800));
 
-
             bindingSource4.ResetBindings(false);
             bindingSource2.ResetBindings(false);
             bindingSource1.ResetBindings(false);
             bindingSource.ResetBindings(false);
-
         }
 
         private void btn_assigneLivraison_Click(object sender, EventArgs e)
@@ -142,19 +139,16 @@ namespace Programme_de_gestion_de_livraison_POO
             bindingSource3.ResetBindings(false);
             bindingSource2.ResetBindings(false);
         }
-
         private void camionneurToolStripMenuItem_Click(object sender, EventArgs e)
         {
             EntreeCamionneur.ShowDialog();
             bindingSource.ResetBindings(false);
         }
-
         private void camionToolStripMenuItem_Click(object sender, EventArgs e)
         {
             EntreeCamion.ShowDialog();
             bindingSource1.ResetBindings(false);
         }
-
         private void livraisonToolStripMenuItem_Click(object sender, EventArgs e)
         {
             EntreeLivraison.ShowDialog();
@@ -165,8 +159,6 @@ namespace Programme_de_gestion_de_livraison_POO
             EntreeVoyage1.ShowDialog();
             bindingSource4.ResetBindings(false);
         }
-
-
         private void lst_voyages_SelectedIndexChanged(object sender, EventArgs e)
         {
             grp_voyageSelectionne.Visible = true;
@@ -177,14 +169,14 @@ namespace Programme_de_gestion_de_livraison_POO
 
             lst_voyage_SelectedItemChange();
         }
-
-
         private void cmb_camionneurs_TextChanged(object sender, EventArgs e)
         {
-            VoyageSelectionnée.Camionneur = cmb_camionneurs.Text;
+            if (cmb_camionneurs.Text != "")
+            {
+                VoyageSelectionnée.Camionneur = cmb_camionneurs.Text;
+            }
+
         }
-
-
         private void cmb_camions_TextChanged(object sender, EventArgs e)
         {
             if (cmb_camions.Text != "")
@@ -193,10 +185,8 @@ namespace Programme_de_gestion_de_livraison_POO
             }
 
         }
-
         private void txt_distance_TextChanged(object sender, EventArgs e)
         {
-
             try
             {
                 int result;
@@ -220,7 +210,6 @@ namespace Programme_de_gestion_de_livraison_POO
             }
 
         }
-
         private void dtp_date_ValueChanged(object sender, EventArgs e)
         {
             try
@@ -242,13 +231,12 @@ namespace Programme_de_gestion_de_livraison_POO
 
 
         }
-
-
         private void VérificationPoidsVolume(livraison LivraisonSelectionéeATransferer, Voyage VoyageSelectionnée, List<livraison> ListeLivraison, int poidTotalLivraisonnIncluse, int volumeTotalLivraisonIncluse)
         {
             int poidTotalAvecLaLivraisonAInclure = LivraisonSelectionéeATransferer.Poids + poidTotalLivraisonnIncluse;
             int volumeTotalAvecLivraisonAInclure = LivraisonSelectionéeATransferer.Volume + volumeTotalLivraisonIncluse;
 
+            
             if (volumeTotalAvecLivraisonAInclure <= VoyageSelectionnée.Camion.Volume && poidTotalAvecLaLivraisonAInclure <= VoyageSelectionnée.Camion.Poids)
             {
                 if (LivraisonSelectionéeATransferer.Poids <= VoyageSelectionnée.Camion.Poids)
@@ -258,6 +246,15 @@ namespace Programme_de_gestion_de_livraison_POO
                         VoyageSelectionnée.Livraisons.Add(LivraisonSelectionéeATransferer);
                         ListeLivraison.Remove(LivraisonSelectionéeATransferer);
                     }
+                    else 
+                    {
+                        MessageBox.Show("Le volume de cette livraison est supérieur au camion choisi");
+                    }
+                }
+                else if (LivraisonSelectionéeATransferer.Poids > VoyageSelectionnée.Camion.Poids)
+                {
+                    MessageBox.Show("Le poid de ce camion est trop petit pour la livraison");
+
                 }
             }
             else if (LivraisonSelectionéeATransferer.Poids > VoyageSelectionnée.Camion.Poids)
@@ -265,20 +262,14 @@ namespace Programme_de_gestion_de_livraison_POO
                 MessageBox.Show("Le poid de ce camion est trop petit pour la livraison");
 
             }
-            else if (LivraisonSelectionéeATransferer.Volume > VoyageSelectionnée.Camion.Volume)
-            {
-                MessageBox.Show("Le volume de cette livraison est supérieur au camion choisi");
-            }
-            
             else
             {
                 MessageBox.Show("La livraison total sera trop grosse pour le camion");
             }
 
-        }
+         
 
-        private void cmb_camions_Click(object sender, EventArgs e)
-        {
+           
 
         }
 
