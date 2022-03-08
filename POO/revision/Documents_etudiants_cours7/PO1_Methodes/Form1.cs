a782cab3ed95048921a94b7cff5dc126942ed27f
Hugo Abric <80226527+TheDevApprentice@users.noreply.github.com>
2022-03-08 16:11:58 -0500

    .

---
+++ POO/revision/Documents_etudiants_cours7/PO1_Methodes/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PO1_Methodes
+{
+    public partial class Form1 : Form
+    {
+        List<Voiture> voituresDeCourse = new List<Voiture>();
+
+        internal List<Voiture> VoituresDeCourse { get => voituresDeCourse; set => voituresDeCourse = value; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Voiture voiture =  new Voiture(40, 0, 0, 0, 2, 10, Color.Red);
+            VoituresDeCourse.Add(voiture);
+            voiture = new Voiture(40, 0, 0, 100, 2, 10, Color.Blue);
+            VoituresDeCourse.Add(voiture); 
+               
+            //VoituresDeCourse.Add(new Voiture(40, 0, 0, 200, 2, 10, Color.Yellow));
+        }
+
+        private void pictureBox_course_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Voiture voiture in VoituresDeCourse)
+            {
+                Bitmap bitmapDeLaVoiture = voiture.Dessiner();
+                e.Graphics.DrawImage(bitmapDeLaVoiture, voiture.PositionX, voiture.PositionY); 
+
+            }
+            
+        }
+
+        private void button_avancer_Click(object sender, EventArgs e)
+        {
+
+            foreach (Voiture voiture in VoituresDeCourse)
+            {
+                voiture.Bouger(5); 
+            }
+
+            pictureBox_course.Invalidate(); 
+        }
+    }
+}
---
+++ POO/revision/Documents_etudiants_cours7/PO1_Methodes/Voiture.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace PO1_Methodes
+{
+    class Voiture
+    {
+        private float quantiteEssenceLitre;
+        private float capaciteResevoirEssenceLitre;
+        private float positionX;
+        private float positionY;
+        private float consommationEssence;
+        private float puissanceMoteur;
+        private Color couleur;
+
+        public Voiture(float quantiteEssenceLitre, float capaciteResevoirEssenceLitre, float positionX, float positionY, float consommationEssence, float puissanceMoteur, Color couleur)
+        {
+            this.QuantiteEssenceLitre = quantiteEssenceLitre;
+            this.CapaciteResevoirEssenceLitre = capaciteResevoirEssenceLitre;
+            this.PositionX = positionX;
+            this.PositionY = positionY;
+            this.ConsommationEssence = consommationEssence;
+            this.PuissanceMoteur = puissanceMoteur;
+            this.Couleur = couleur;
+        }
+
+        public float QuantiteEssenceLitre { get => quantiteEssenceLitre; set => quantiteEssenceLitre = value; }
+        public float CapaciteResevoirEssenceLitre { get => capaciteResevoirEssenceLitre; set => capaciteResevoirEssenceLitre = value; }
+
+   
+
+        public float PositionX { get => positionX; private set => positionX = value; }
+        public float PositionY { get => positionY; set => positionY = value; }
+        public float ConsommationEssence { get => consommationEssence; set => consommationEssence = value; }
+        public float PuissanceMoteur { get => puissanceMoteur; set => puissanceMoteur = value; }
+        public Color Couleur { get => couleur; set => couleur = value; }
+
+       public float Bouger(float nombreDeSecondes)
+        {
+            float kmParcourus = 0;
+
+            if (quantiteEssenceLitre > nombreDeSecondes * consommationEssence)
+            {
+                // assez d'essence 
+                kmParcourus = nombreDeSecondes * puissanceMoteur;
+                quantiteEssenceLitre = quantiteEssenceLitre - (nombreDeSecondes * consommationEssence);
+                
+
+            }
+            else
+            {
+                // manque d'essence
+
+                float tempsPossibleAvecEssenceDisponible = quantiteEssenceLitre / consommationEssence;
+                kmParcourus = tempsPossibleAvecEssenceDisponible * puissanceMoteur;
+                quantiteEssenceLitre = 0;
+
+                
+            }
+
+            positionX += kmParcourus;
+            return kmParcourus; 
+        }
+
+        public Bitmap Dessiner()
+        {
+            Bitmap bitmapVoiture = new Bitmap(200,200);
+
+            using (Graphics graphic = Graphics.FromImage(bitmapVoiture))
+            {
+                Rectangle carosserie = new Rectangle(60,100,80,50);
+                Rectangle roueArrierre = new Rectangle(25, 150, 25, 25);
+                Rectangle roueAvant = new Rectangle(140, 150, 25, 25);
+                Brush brushCarroserie = new SolidBrush(Couleur);
+                Brush brushRoues = Brushes.Black;
+
+                graphic.FillRectangle(brushCarroserie, carosserie);
+                graphic.FillEllipse(brushRoues, roueArrierre);
+                graphic.FillEllipse(brushRoues, roueAvant);
+            }
+
+
+            return bitmapVoiture; 
+        }
+        
+
+    }
+}
